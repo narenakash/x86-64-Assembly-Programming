7448d57f815800c858891d75fe4e2305645070e8
Naren Akash R J <43748290+narenakash@users.noreply.github.com>
2019-06-30 22:27:58 +0530

    Task 02 Assembly Code Uploaded

---
+++ Task 02/qn2.s
@@ -0,0 +1,48 @@
+# x86 Assembly Code for Finding the Nth Fibonacci Number
+
+.extern printf
+
+.data
+N:
+	.byte 10
+first:
+	.int
+second:
+	.int
+temp:
+	.int
+.form:
+	.string "%d"
+
+.text
+.global main
+main:
+
+# Result will be found in %ebx register at the end
+
+	movl $0,%eax      # temp   <--> %eax
+	movl $0,%ebx      # first  <--> %ebx
+	movl $1,%ecx	  # second <--> %ecx
+	mov N@GOTPCREL(%rip), %rdx
+	movzbl (%rdx), %edx
+.loop:
+	cmpl $0,%edx
+	je .loopexit
+	movl %ebx,%eax    # temp = first
+	addl %ecx,%eax	  # temp += second
+	movl %ecx,%ebx	  # first = second
+	movl %eax,%ecx	  # second = temp
+	dec %edx	  # N -= 1
+	jmp .loop
+.loopexit:
+	movl %ebx,%esi
+	# movq form@GOTPCREL, %rdi
+	leaq .form(%rip), %rdi 
+	call printf
+	ret
+
+# Naren Akash,R J - 2018111020
+# February 27, 2019 - Wednesday
+# International Institute of Information Technology, Hyderabad
+
+
